c3268df20b8a6a19a0d2846362e377431f165cf3
Zoe Ames <zoeames@gmail.com>
2014-12-29 10:47:05 -0600

    all pages/routes added on front end

--- client/index.js
+++ client/index.js
@@ -6,9 +6,17 @@
       $urlRouterProvider.otherwise('/');
 
       $stateProvider
-        .state('home',         {url:'/',         templateUrl:'/views/home/home.html'})
-        .state('register',     {url:'/register', templateUrl:'/views/users/users.html',        controller:'UsersCtrl'})
-        .state('login',        {url:'/login',    templateUrl:'/views/users/users.html',        controller:'UsersCtrl'});
+        .state('home',         {url:'/',                                        templateUrl:'/views/home/home.html'})
+        .state('register',     {url:'/register',                                templateUrl:'/views/users/users.html',            controller:'UsersCtrl'})
+        .state('login',        {url:'/login',                                   templateUrl:'/views/users/users.html',            controller:'UsersCtrl'})
+        .state('newcourse',    {url:'/newcourse',                               templateUrl:'/views/courses/newcourse.html',      controller:'CoursesCtrl'})
+        .state('course',       {url:'/courses/{courseId}',                      templateUrl:'/views/courses/course_detail.html',  controller:'CoursesCtrl'})
+        .state('newlesson',    {url:'/courses/{courseId}/newlesson',            templateUrl:'/views/courses/newlesson.html',      controller:'CoursesCtrl'})
+        .state('grades',       {url:'/courses/{courseId}/grades',               templateUrl:'/views/courses/grades.html',         controller:'CoursesCtrl'})
+        .state('lesson',       {url:'/courses/{courseId}/{lessonId}',           templateUrl:'/views/courses/lesson_detail.html',  controller:'CoursesCtrl'})
+        .state('newquiz',      {url:'/courses/{courseId}/{lessonId}/newquiz',   templateUrl:'/views/courses/newquiz.html',        controller:'CoursesCtrl'})
+        .state('quiz',         {url:'/courses/{courseId}/{lessonId}/{quizId}',  templateUrl:'/views/courses/quiz_detail.html',    controller:'CoursesCtrl'});
+
       }])
     .run(['$rootScope', '$http', function($rootScope, $http){
       $http.get('/status').then(function(response){
---
+++ client/views/courses/courses.js
@@ -0,0 +1,8 @@
+(function(){
+  'use strict';
+
+  angular.module('hapi-auth')
+    .controller('CoursesCtrl', ['$rootScope', '$scope', function($rootScope, $scope){
+        $scope.newcourse = {};
+    }]);
+})();

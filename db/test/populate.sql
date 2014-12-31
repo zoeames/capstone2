bea81c25b4e307120241720ff616129cad9f5570
Zoe Ames <zoeames@gmail.com>
2014-12-31 15:59:47 -0600

    travis

--- db/test/populate.sql
+++ db/test/populate.sql
@@ -1,7 +1,6 @@
 insert into users (institutionid, password, avatar, token, firstname, lastname, email, isadmin)  values ('1111', 'b', 'c', 'd', 'e', 'f', 'g', 'false');
 insert into users (institutionid, password, avatar, token, firstname, lastname, email, isadmin)  values ('1111', 'b', 'c', 'd', 'e', 'f', 'g', 'true');
 insert into users (institutionid, password, avatar, token, firstname, lastname, email, isadmin)  values ('1111', 'b', 'c', 'd', 'e', 'f', 'g', 'false');
-insert into users (institutionId,password,avatar,token) values ('a3', 'b', 'c', 'd');
 
 delete from users;
 

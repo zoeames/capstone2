b0c41e351ce139f83937dc471ed65c7177b4192c
Zoe Ames <zoeames@gmail.com>
2015-01-04 18:09:21 -0600

    newcourse form created

--- client/views/courses/courses.js
+++ client/views/courses/courses.js
@@ -4,5 +4,15 @@
   angular.module('hapi-auth')
     .controller('CoursesCtrl', ['$rootScope', '$scope', function($rootScope, $scope){
         $scope.newcourse = {};
+        $scope.topics = [
+          {name:'Astronomy'},
+          {name:'Physics'},
+          {name:'Biology'},
+          {name:'Chemistry'},
+          {name:'Math'},
+          {name:'Foreign Language'},
+          {name:'History'},
+          {name:'Humanities'},
+          {name:'Other'}];
     }]);
 })();

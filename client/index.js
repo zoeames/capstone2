8677bc5288d2e61ea08c47d308c4aaef416f421b
Zoe Ames <zoeames@gmail.com>
2015-01-06 23:39:30 -0600

    course detail page created

--- client/index.js
+++ client/index.js
@@ -6,16 +6,17 @@
       $urlRouterProvider.otherwise('/');
 
       $stateProvider
-        .state('profile',      {url:'/',                                        templateUrl:'/views/profile/profile.html',        controller:'ProfileCtrl'})
-        .state('register',     {url:'/register',                                templateUrl:'/views/users/users.html',            controller:'UsersCtrl'})
-        .state('login',        {url:'/login',                                   templateUrl:'/views/users/users.html',            controller:'UsersCtrl'})
-        .state('newcourse',    {url:'/newcourse',                               templateUrl:'/views/courses/newcourse.html',      controller:'CoursesCtrl'})
-        .state('course',       {url:'/courses/{courseId}',                      templateUrl:'/views/courses/course_detail.html',  controller:'CoursesCtrl'})
-        .state('newlesson',    {url:'/courses/{courseId}/newlesson',            templateUrl:'/views/courses/newlesson.html',      controller:'CoursesCtrl'})
-        .state('grades',       {url:'/courses/{courseId}/grades',               templateUrl:'/views/courses/grades.html',         controller:'CoursesCtrl'})
-        .state('lesson',       {url:'/courses/{courseId}/{lessonId}',           templateUrl:'/views/courses/lesson_detail.html',  controller:'CoursesCtrl'})
-        .state('newquiz',      {url:'/courses/{courseId}/{lessonId}/newquiz',   templateUrl:'/views/courses/newquiz.html',        controller:'CoursesCtrl'})
-        .state('quiz',         {url:'/courses/{courseId}/{lessonId}/{quizId}',  templateUrl:'/views/courses/quiz_detail.html',    controller:'CoursesCtrl'});
+        .state('profile',      {url:'/',                                                       templateUrl:'/views/profile/profile.html',        controller:'ProfileCtrl'})
+        .state('register',     {url:'/register',                                               templateUrl:'/views/users/users.html',            controller:'UsersCtrl'})
+        .state('login',        {url:'/login',                                                  templateUrl:'/views/users/users.html',            controller:'UsersCtrl'})
+        .state('newcourse',    {url:'/newcourse',                                              templateUrl:'/views/courses/newcourse.html',      controller:'CoursesCtrl'})
+        .state('course',       {url:'/courses/{courseId}',                                     templateUrl:'/views/courses/course_detail.html',  controller:'CoursesCtrl'})
+        .state('newlesson',    {url:'/courses/{courseId}/newlesson',                           templateUrl:'/views/courses/newlesson.html',      controller:'CoursesCtrl'})
+        .state('grades',       {url:'/courses/{courseId}/grades',                              templateUrl:'/views/courses/grades.html',         controller:'CoursesCtrl'})
+        .state('lesson',       {url:'/courses/{courseId}/lessons/{lessonId}',                  templateUrl:'/views/courses/lesson_detail.html',  controller:'CoursesCtrl'})
+        .state('newquiz',      {url:'/courses/{courseId}/lessons/{lessonId}/newquiz',          templateUrl:'/views/courses/newquiz.html',        controller:'CoursesCtrl'})
+        .state('quiz',         {url:'/courses/{courseId}/lessons/{lessonId}/quizes/{quizId}',  templateUrl:'/views/courses/quiz_detail.html',    controller:'CoursesCtrl'});
+
 
       }])
     .run(['$rootScope', '$http', function($rootScope, $http){

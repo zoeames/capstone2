457191ac56c1dd11b5a029981b4339b50761f429
Zoe Ames <zoeames@gmail.com>
2015-01-10 02:46:17 -0600

    toastr added to quiz form.  form redirects to lesson detail page

--- client/views/quizes/quizes.js
+++ client/views/quizes/quizes.js
@@ -3,6 +3,8 @@
 
   angular.module('hapi-auth')
     .controller('QuizCtrl', ['$rootScope', '$scope', '$state', 'Quiz', function($rootScope, $scope, $state, Quiz){
+      $scope.courseId = $state.params.courseId;
+      $scope.lessonId = $state.params.lessonId;
       $scope.files = [];
       $scope.count=0;
 
@@ -15,6 +17,11 @@
           console.log('THIS IS THE response.data >>>>>', response.data);
           $scope.quiz = {};
           Quiz.upload(response.data.quizId, $scope.files);
+          toastr.success('Quiz successfully added.');
+          $state.go('lesson', {courseId:$scope.courseId, lessonId:$scope.lessonId});
+          }, function(){
+            toastr.error('Error creating quiz, try again.');
+            console.log('error');
         });
       };
 

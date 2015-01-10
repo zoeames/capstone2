c7a7e64f5d0878d479ad6f59247566c2027ff44f
Zoe Ames <zoeames@gmail.com>
2015-01-10 03:31:06 -0600

    image added to quiz detail

--- db/functions/show_quiz.sql
+++ db/functions/show_quiz.sql
@@ -1,11 +1,12 @@
 create or replace function show_quiz (qid integer)
-returns table ("quizId" integer, "quizTitle" varchar, "quizQuestion" text, "answerA" varchar, "answerB" varchar, "answerC" varchar, "answerD" varchar, "answerE" varchar, "correctAnswer" varchar, "isActive" boolean, "isCompleted" boolean) AS $$
+returns table ("quizId" integer, "quizTitle" varchar, "quizQuestion" text, "answerA" varchar, "answerB" varchar, "answerC" varchar, "answerD" varchar, "answerE" varchar, "correctAnswer" varchar, "isActive" boolean, "isCompleted" boolean, "photoURL" varchar) AS $$
 declare
 begin
 
   return query
-    select q.id as "quizId", q.quiztitle as "quizTitle", q.question as "quizQuestion", q.answera as "answerA", q.answerb as "answerB", q.answerc as "answerC", q.answerd as "answerD", q.answere as "answerE", q.correctanswer as "correctAnswer", q.isactive as "isActive", q.iscompleted as "isCompleted"
+    select q.id as "quizId", q.quiztitle as "quizTitle", q.question as "quizQuestion", q.answera as "answerA", q.answerb as "answerB", q.answerc as "answerC", q.answerd as "answerD", q.answere as "answerE", q.correctanswer as "correctAnswer", q.isactive as "isActive", q.iscompleted as "isCompleted", p.url as "photoURL"
     from quizzes q
+    inner join photos p on p.quizid = q.id
     where q.id = qid;
 
 

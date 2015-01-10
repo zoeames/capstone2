a7da2f6a88bed5d884ec6539b736313db6dd8b8a
Zoe Ames <zoeames@gmail.com>
2015-01-10 03:22:29 -0600

    buttons added for answers in quiz detail

--- db/functions/show_quiz.sql
+++ db/functions/show_quiz.sql
@@ -1,10 +1,10 @@
 create or replace function show_quiz (qid integer)
-returns table ("quizId" integer, "quizTitle" varchar, "answerA" varchar, "answerB" varchar, "answerC" varchar, "answerD" varchar, "answerE" varchar, "correctAnswer" varchar, "isActive" boolean, "isCompleted" boolean) AS $$
+returns table ("quizId" integer, "quizTitle" varchar, "quizQuestion" text, "answerA" varchar, "answerB" varchar, "answerC" varchar, "answerD" varchar, "answerE" varchar, "correctAnswer" varchar, "isActive" boolean, "isCompleted" boolean) AS $$
 declare
 begin
 
   return query
-    select q.id as "quizId", q.quiztitle as "quizTitle", q.answera as "answerA", q.answerb as "answerB", q.answerc as "answerC", q.answerd as "answerD", q.answere as "answerE", q.correctanswer as "correctAnswer", q.isactive as "isActive", q.iscompleted as "isCompleted"
+    select q.id as "quizId", q.quiztitle as "quizTitle", q.question as "quizQuestion", q.answera as "answerA", q.answerb as "answerB", q.answerc as "answerC", q.answerd as "answerD", q.answere as "answerE", q.correctanswer as "correctAnswer", q.isactive as "isActive", q.iscompleted as "isCompleted"
     from quizzes q
     where q.id = qid;
 

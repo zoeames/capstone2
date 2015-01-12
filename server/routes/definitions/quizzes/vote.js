224a07dd304ecfa9dbe2d87f19ac9d513a96636f
Zoe Ames <zoeames@gmail.com>
2015-01-11 18:17:25 -0600

    capital fixed for heroku

--- server/routes/definitions/quizzes/vote.js
+++ server/routes/definitions/quizzes/vote.js
@@ -1,7 +1,7 @@
 'use strict';
 
 var Joi  = require('joi'),
-    Quiz = require('../../../models/Quiz');
+    Quiz = require('../../../models/quiz');
 
 module.exports = {
   description: 'Submits vote',

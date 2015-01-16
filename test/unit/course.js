11258ef14c85cae1f255f39b2542a43de93a6d87
Zoe Ames <zoeames@gmail.com>
2015-01-16 16:57:33 -0600

    unit testing for courses.  constructor, create, show

---
+++ test/unit/course.js
@@ -0,0 +1,50 @@
+/* jshint expr:true */
+
+'use strict';
+
+var expect     = require('chai').expect,
+    cp         = require('child_process'),
+    h          = require('../helpers/helpers'),
+    Course       = require('../../server/models/course'),
+    Lab        = require('lab'),
+    lab        = exports.lab = Lab.script(),
+    describe   = lab.describe,
+    it         = lab.it,
+    beforeEach = lab.beforeEach,
+    db         = h.getdb();
+
+describe('User', function(){
+  beforeEach(function(done){
+    cp.execFile(__dirname + '/../scripts/clean-db.sh', [db], {cwd:__dirname + '/../scripts'}, function(err, stdout, stderr){
+      done();
+    });
+  });
+
+  describe('constructor', function(){
+    it('should create a Course object', function(done){
+      var course = new Course({userId:'1'});
+
+      expect(course).to.be.instanceof(Course);
+      done();
+    });
+  });
+
+  describe('.create', function(){
+    it('should create a course', function(done){
+      Course.create({id:1}, {InstructorId: '1', courseTitle: 'Phys102: intro to physics', institutionId:'234235', semester: 'Fall 2015', webpage:'www.google.com', topic:'Astronomy'}, function(err, results){
+        expect(err).to.be.null;
+        expect(results).to.be.above(0);
+        done();
+      });
+    });
+  });
+  describe('.show', function(){
+    it('should show a course', function(done){
+      Course.show(1, function(err, results){
+        expect(err).to.be.null;
+        expect(results.courseTitle).to.equal('Phys101: intro to physics');
+        done();
+      });
+    });
+  });
+});
